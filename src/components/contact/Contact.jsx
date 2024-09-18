46c82dfff0991eadcd2cb7eecc20e65ff254dc23
ashvini8 <ashvinihunagund@gmail.com>
2024-09-18 15:29:02 -0400

    added new project, fixed contact me and added toast notif

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,26 +4,44 @@ import { IoMail } from "react-icons/io5"
 import { FaPhoneAlt, FaLinkedin, FaGithub } from "react-icons/fa";
 import { useRef } from "react"
 import emailjs from 'emailjs-com'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
   const formRef = useRef()
 
-  const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_rqlv2jl', 'template_s70uktr', formRef.current, 'LS2O5CpIW98YQ7qFT')
+    emailjs.sendForm('service_8jxkqlk', 'template_s70uktr', formRef.current, 'LS2O5CpIW98YQ7qFT')
       .then((result) => {
           console.log(result.text);
+          toast.success('Message sent successfully!', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
           formRef.current.reset(); 
       }, (error) => {
           console.log(error.text);
+          toast.error('Failed to send message. Please try again.', {
+            position: "top-center",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
       });
-
   }
 
   return (
     <div className='c' id='contact'>
-      {/* <div className="c_bg"></div> */}
       <div className="c_wrapper">
         <div className="c_left">
           <h1 className="c_title">Contact Me!</h1>
@@ -32,28 +50,28 @@ const Contact = () => {
             {/* email */}
             <div className="c_info_item">
               <IoMail className='c_icon'/>
-              <h6>email: </h6>
+              <h6>Email: </h6>
               <p>ashvinihunagund@gmail.com</p>
             </div>
 
             {/* phone number */}
             <div className="c_info_item">
               <FaPhoneAlt className='c_icon'/>
-              <h6>number: </h6>
+              <h6>Number: </h6>
               <p>905-251-7675</p>
             </div>
 
             {/* linkedin */}
             <div className="c_info_item">
               <FaLinkedin className='c_icon'/>
-              <h6>linkedin: </h6>
+              <h6>LinkedIn: </h6>
               <p><a href="https://www.linkedin.com/in/ashvinihunagund/">linkedin.com/in/ashvinihunagund/</a></p>
             </div>
 
             {/* github */}
             <div className="c_info_item">
               <FaGithub className='c_icon'/>
-              <h6>github: </h6>
+              <h6>GitHub: </h6>
               <p><a href="https://github.com/ashvini8">github.com/ashvini8/</a></p>
             </div>
 
@@ -62,17 +80,16 @@ const Contact = () => {
 
         <div className="c_right">
           <form ref={formRef} onSubmit={handleSubmit}>
-            <input type ="text" placeholder="Name" name="user_name"/>
-            <input type ="text" placeholder="Email" name="user_email"/>
-            <textarea rows="5" type='text' placeholder="Message" name="user_message"></textarea>
-            <button>Send</button>
-
+            <input type="text" placeholder="Name" name="user_name" required />
+            <input type="text" placeholder="Email" name="user_email" required />
+            <textarea rows="5" type='text' placeholder="Message" name="user_message" required></textarea>
+            <button type="submit">Send</button>
           </form>
         </div>
-
       </div>
+      <ToastContainer />
     </div>
   )
 }
 
-export default Contact
+export default Contact;
--- src/data.js
+++ src/data.js
@@ -3,33 +3,40 @@ import systemTools from "./img/systemTools.png"
 import coursePath from "./img/coursePath.png"
 import mastermind from "./img/mastermind.png"
 import hangman from "./img/hangman.png"
+import gohere from "./img/goHere.png"
 export const projects = [
   {
     id: 1,
     name: "Portfolio Website",
     img: website,
-    link: "https://github.com/ashvini8?tab=repositories",
+    link: "https://github.com/ashvini8/ashvinihunagund",
   },
   {
     id: 2,
+    name: "GoHere Mobile App",
+    img: gohere,
+    link: "https://github.com/chrisnathan20/c01w24-project-39Washrooms",
+  },
+  {
+    id: 3,
     name: "System Monitoring Tool Linux",
     img: systemTools,
     link: "https://github.com/ashvini8/linuxSystemMonitoringTool",
   },
   {
-    id: 3,
+    id: 4,
     name: "CoursePath Android App",
     img: coursePath,
     link: "https://github.com/Nafizul02/ASProject",
   },
   {
-    id: 4,
+    id: 5,
     name: "Mastermind Website",
     img: mastermind,
     link: "https://github.com/ashvini8/MastermindWebsite",
   },
   {
-    id: 5,
+    id: 6,
     name: "Hangman Game Website",
     img: hangman,
     link: "https://github.com/ashvini8/hangmanWesbite",
